70db832cc4dc3ba337e8d3a876e0868ea237b8fe
Eben <kwabenat679@gmail.com>
2025-02-18 23:53:25 +0000

    added quotes in index,js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Random Quote Generator</title>
-</head>
-<body>
-    <h1>Random Quote Generator</h1>
-    <p id="quote"></p>
-    <button>Generate Quote</button>
-</body>
-</html>
+const quotes = [
+    "Wiase y3 den",
+    "Nipa y3 bad",
+    "Ohia y3 ya",
+    "Life is full of ups and downs",
+    "Trust God",
+    "You win when you help others win"
+]
